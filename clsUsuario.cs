e1a102febf4d43680e89a506bdb3177d790c5fec
Alumno <Alumno@PC33>
2024-05-24 13:31:11 -0300

    Agregar archivos de proyecto.

---
+++ clsUsuario.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryTP2
+{
+    internal class clsUsuario
+    {
+        OleDbConnection conexionBD;
+        OleDbCommand comandoBD;
+        OleDbDataReader lectorBD;
+
+        OleDbDataAdapter adaptadorBD;
+        DataSet objDS;
+
+        public string estadoConexion;
+
+        public clsUsuario()
+        {
+            try
+            {
+
+                conexionBD = new OleDbConnection();
+                conexionBD.ConnectionString = "Provider=Microsoft.JET.OLEDB.4.0;Data Source=BDusuarios.accdb"; //puse la db en bin/debug
+                conexionBD.Open();
+
+                objDS = new DataSet();
+
+                estadoConexion = "Conectado";
+            }
+            catch (Exception error)
+            {
+                estadoConexion = error.Message;
+            }
+        }
+
+        public void RegistroLogInicioSesion()
+        {
+            try
+            {
+                comandoBD = new OleDbCommand();
+
+                comandoBD.Connection = conexionBD;
+                comandoBD.CommandType = CommandType.TableDirect;
+                comandoBD.CommandText = "Logs";
+
+                adaptadorBD = new OleDbDataAdapter(comandoBD);
+
+                adaptadorBD.Fill(objDS, "Logs");
+
+                DataTable objTabla = objDS.Tables["Logs"];
+                DataRow nuevoRegistro = objTabla.NewRow();
+
+                nuevoRegistro["Categoria"] = "Inicio Sesión";
+                nuevoRegistro["FechaHora"] = DateTime.Now;
+                nuevoRegistro["Descripcion"] = "Inicio exitoso";
+
+                objTabla.Rows.Add(nuevoRegistro);
+
+                OleDbCommandBuilder constructor = new OleDbCommandBuilder(adaptadorBD);
+                adaptadorBD.Update(objDS, "Logs");
+
+                estadoConexion = "Registro exitoso de log";
+            }
+            catch (Exception error)
+            {
+
+                estadoConexion = error.Message;
+            }
+
+        }
+
+        public void ValidarUsuario(string nombreUser, string passUser)
+        {
+            try
+            {
+                comandoBD = new OleDbCommand();
+
+                comandoBD.Connection = conexionBD;
+                conexionBD.Open();
+                comandoBD.CommandType = CommandType.TableDirect;
+                comandoBD.CommandText = "Usuario";
+
+                lectorBD = comandoBD.ExecuteReader();
+
+                if (lectorBD.HasRows)
+                {
+                    while (lectorBD.Read())
+                    {
+                        if (lectorBD[1].ToString() == nombreUser && lectorBD[2].ToString() == passUser)
+                        {
+                            estadoConexion = "Usuario EXISTE";
+                        }
+                    }
+                }
+                conexionBD.Close();
+            }
+            catch (Exception error)
+            {
+
+                estadoConexion = error.Message;
+            }
+        }
+    }
+}
